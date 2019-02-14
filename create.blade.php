38ae2c6e1fc0f009f68c2def5417a55aa0e8bb24
Kentaro Arai <k.arai0820@gmail.com>
2019-02-15 00:22:29 +0900

    PHP/Laravel11 task

---
+++ create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MYProfile</title>
+    </head>
+    <body>
+      <h1>MYProfile</h1>
+      <p>ここに私のプロフィール文章が入ります。ここに私のプロフィール文章が入ります。ここに私のプロフィール文章が入ります。</p>
+      
+      <p>趣味</p>
+      <ul>
+      	<li>観劇</li>
+      	<li>ボルダリング</li>
+      	<li>旅行</li>
+      </ul>	
+
+    </body>
+</html>
---
+++ edit.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MYProfile</title>
+    </head>
+    <body>
+      <h1>MyProfile　Edit</h1>
+      <p>これはエディット画面ですこれはエディット画面ですこれはエディット画面ですこれはエディット画面です</p>
+      
+
+    </body>
+</html>
